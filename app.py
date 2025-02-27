d7ebfa4a7eddabef4cf697fcb806b1d7e77a91bc
Bebbe <bebbe.vr@gmail.com>
2025-02-27 14:10:18 +0200

    streamlit

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 import streamlit as st
-import joblib  # Används för att ladda pkl-filer
+import requests # HTTP requests
 
-# Ladda modellen
-model = joblib.load("sentiment_model.pkl")
-
-# Funktion för att göra sentimentanalys
-def predict_sentiment(text):
-    sentiment = model.predict([text])
-    return "POSITIVE" if sentiment[0] == 1 else "NEGATIVE"
-
-# Streamlit UI
 st.title("Sentiment Analysis App")
-st.write("Skriv in en text och få dess sentiment!")
+st.write("Write text and get sentiment analysis!!")
 
-# Textinput från användaren
-user_input = st.text_area("Skriv din text här:")
+user_input = st.text_area("Write text:")
 
-# Knapp för analys
-if st.button("Analysera sentiment"):
+if st.button("Analyze sentiment"):
     if user_input:
-        sentiment = predict_sentiment(user_input)
-        st.write(f"**Resultat:** {sentiment}")
-    else:
-        st.warning("Vänligen skriv in en text för att analysera!")
+        response = requests.post(
+            "https://module3-assignment-part2-api.onrender.com/predict",
+            json={"text": user_input},
+        )
+        if response.status_code == 200:
+            sentiment = response.json().get("sentiment")
+            st.write(f"**Result:** {sentiment}")
